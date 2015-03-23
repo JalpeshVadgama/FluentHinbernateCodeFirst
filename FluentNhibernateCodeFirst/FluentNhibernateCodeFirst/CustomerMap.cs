decb5985a5cce59673eabc3508a6c43f91a96dc6
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-03-24 00:52:08 +0530

    Added Database Creation Code

--- FluentNhibernateCodeFirst/FluentNhibernateCodeFirst/CustomerMap.cs
+++ FluentNhibernateCodeFirst/FluentNhibernateCodeFirst/CustomerMap.cs
@@ -11,4 +11,6 @@ namespace FluentNhibernateCodeFirst
             Map(c => c.LastName);
         }
     }
+
+    
 }
--- FluentNhibernateCodeFirst/FluentNhibernateCodeFirst/Program.cs
+++ FluentNhibernateCodeFirst/FluentNhibernateCodeFirst/Program.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+
+
 
 namespace FluentNhibernateCodeFirst
 {
     class Program
     {
+        private static ISessionFactory _sessionFactory;
+
         static void Main(string[] args)
         {
+
+        }
+
+        static void CreateDatabase(string connectionString)
+        {
+            var configuration = Fluently.Configure()
+                .Database(MsSqlConfiguration.MsSql2012.ConnectionString(connectionString).ShowSql)
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<CustomerMap>())
+                .BuildConfiguration();
+
+            var exporter = new SchemaExport(configuration);   
+            exporter.Execute(true, true, false);
+
+            _sessionFactory = configuration.BuildSessionFactory();  
         }
     }
+
+    
 }
