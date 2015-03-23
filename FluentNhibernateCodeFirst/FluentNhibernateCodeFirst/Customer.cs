aef9735db6e1d8b23dad1fbb21fa554542c80e54
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-03-24 00:40:24 +0530

    Added Customer Class and Customer Map class

---
+++ FluentNhibernateCodeFirst/FluentNhibernateCodeFirst/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace FluentNhibernateCodeFirst
+{
+    public class Customer
+    {
+        public virtual int CustomerId { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+    } 
+}
---
+++ FluentNhibernateCodeFirst/FluentNhibernateCodeFirst/CustomerMap.cs
@@ -0,0 +1,14 @@
+﻿using FluentNHibernate.Mapping;
+
+namespace FluentNhibernateCodeFirst
+{
+    public class CustomerMap : ClassMap<Customer>
+    {
+        public CustomerMap()
+        {
+            Id(c => c.CustomerId);
+            Map(c => c.FirstName);
+            Map(c => c.LastName);
+        }
+    }
+}
